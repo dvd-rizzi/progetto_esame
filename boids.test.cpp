d33bdb34f706e6972daee0d6e515867778cb2faf
Mattia-Bruce <mattia.brunelli3@studio.unibo.it>
2025-05-11 15:37:37 +0200

    aggiunto primo test

--- boids.test.cpp
+++ boids.test.cpp
@@ -4,10 +4,11 @@
 using namespace project;
 
 TEST_CASE("Checking the lower distance function") {
-  boid boid1{0., 0., 1., 1.};
-  SUBCASE("second boid way farther than lower distance") {
-    boid boid2{10., 10., 1., 1.};
-   std::vector<boid> flockvect = {boid1, boid2};
-    boids_flock flock{2, flockvect, 50., 5., 0., 0., 0.};
-  }
+    boid boid1{0.,0.,1.,1.};
+    SUBCASE("second boid way farther than lower distance") {
+        boid boid2{10.,10.,1.,1.};
+        std::vector<boid> flockvect={boid1,boid2};
+        boids_flock flock{ 2,flockvect,50.,5.,0.,0.,0.};
+        CHECK(flock.lower_distance(boid1,boid2)==true);
+    }
 }
