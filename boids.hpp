a06a292a6f8099786f779df2c5dea9bea6226175
Davide <davide.rizzi4@studio.unibo.it>
2025-05-12 23:15:50 +0200

    Formattazione con clang-format

--- boids.hpp
+++ boids.hpp
@@ -2,53 +2,50 @@
 #define BOIDS_HPP
 #include <vector>
 
+namespace project {
 
-namespace project{
-
-struct boid{
-    double x_position;
-    double y_position;
-    double v_x;
-    double v_y;
+struct boid {
+  double x_position;
+  double y_position;
+  double v_x;
+  double v_y;
 };
 
-bool operator!=(boid a, boid b); 
+bool operator!=(boid a, boid b);
 
-class boids_flock{
-    int N_;
-    std::vector<boid> flock_;
-    double d_;
-    double ds_;
-    double s_;
-    double a_;
-    double c_;
-    
-    public:
+class boids_flock {
+  int N_;
+  std::vector<boid> flock_;
+  double d_;
+  double ds_;
+  double s_;
+  double a_;
+  double c_;
 
-    boids_flock(int N, std::vector<boid> flock, double d, double ds, double s, double a, double c) : N_{N}, flock_{flock}, d_{d}, ds_{ds}, s_{s}, a_{a}, c_{c} {}
-    
-    boid boid_initialize();
-    
-    void flock_formation();
-    
-    bool upper_distance(boid a, boid b);
-    
-    bool lower_distance(boid a, boid b);
+ public:
+  boids_flock(int N, std::vector<boid> flock, double d, double ds, double s, double a, double c) : N_{N}, flock_{flock}, d_{d}, ds_{ds}, s_{s}, a_{a}, c_{c} {}
 
-    double reciprocal_distance_x(boid a, boid b);
+  boid boid_initialize();
 
-    double reciprocal_distance_y(boid a, boid b);
+  void flock_formation();
 
-    double separation_rule_x(boid a);
+  bool upper_distance(boid a, boid b);
 
-    double separation_rule_y(boid a);
+  bool lower_distance(boid a, boid b);
 
-    double alignment_rule_x(boid a, boid b);
+  double reciprocal_distance_x(boid a, boid b);
 
-    double alignment_rule_y(boid a, boid b);
+  double reciprocal_distance_y(boid a, boid b);
 
+  double separation_rule_x(boid a);
+
+  double separation_rule_y(boid a);
+
+  double alignment_rule_x(boid a, boid b);
+
+  double alignment_rule_y(boid a, boid b);
 };
 
-}
+}  // namespace project
 
 #endif
--- boids.test.cpp
+++ boids.test.cpp
@@ -1,98 +1,99 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "boids.hpp"
+
 #include "doctest.h"
 using namespace project;
 
 TEST_CASE("Checking the lower distance function") {
-    boid boid1{0.,0.,1.,1.};
-    SUBCASE("second boid way farther than lower distance") {
-        boid boid2{10.,10.,1.,1.};
-        std::vector<boid> flockvect={boid1,boid2};
-        boids_flock flock{2,flockvect,50.,5.,0.,0.,0.};
-        CHECK(flock.lower_distance(boid1,boid2)==true);
-    }
-    SUBCASE("second boid very close to lower distance") {
-        boid boid2{5.,1.,1.,1.};
-        std::vector<boid> flockvect={boid1,boid2};
-        boids_flock flock{2,flockvect,50.,5.,0.,0.,0.};
-        CHECK(flock.lower_distance(boid1,boid2)==true);
-    }
-    SUBCASE("second boid on lower distance") {
-        boid boid2{5.,0.,1.,1.};
-        std::vector<boid> flockvect={boid1,boid2};
-        boids_flock flock{2,flockvect,50.,5.,0.,0.,0.};
-        CHECK(flock.lower_distance(boid1,boid2)==false);
-    }
-    SUBCASE("second boid closer than lower distance") {
-        boid boid2{1.,1.,1.,1.};
-        std::vector<boid> flockvect={boid1,boid2};
-        boids_flock flock{2,flockvect,50.,5.,0.,0.,0.};
-        CHECK(flock.lower_distance(boid1,boid2)==false);
-    }
+  boid boid1{0., 0., 1., 1.};
+  SUBCASE("second boid way farther than lower distance") {
+    boid boid2{10., 10., 1., 1.};
+    std::vector<boid> flockvect = {boid1, boid2};
+    boids_flock flock{2, flockvect, 50., 5., 0., 0., 0.};
+    CHECK(flock.lower_distance(boid1, boid2) == true);
+  }
+  SUBCASE("second boid very close to lower distance") {
+    boid boid2{5., 1., 1., 1.};
+    std::vector<boid> flockvect = {boid1, boid2};
+    boids_flock flock{2, flockvect, 50., 5., 0., 0., 0.};
+    CHECK(flock.lower_distance(boid1, boid2) == true);
+  }
+  SUBCASE("second boid on lower distance") {
+    boid boid2{5., 0., 1., 1.};
+    std::vector<boid> flockvect = {boid1, boid2};
+    boids_flock flock{2, flockvect, 50., 5., 0., 0., 0.};
+    CHECK(flock.lower_distance(boid1, boid2) == false);
+  }
+  SUBCASE("second boid closer than lower distance") {
+    boid boid2{1., 1., 1., 1.};
+    std::vector<boid> flockvect = {boid1, boid2};
+    boids_flock flock{2, flockvect, 50., 5., 0., 0., 0.};
+    CHECK(flock.lower_distance(boid1, boid2) == false);
+  }
 }
 
 TEST_CASE("Checking the upper distance function") {
-    boid boid1{0.,0.,1.,1.};
-    SUBCASE("second boid way farther than upper distance") {
-        boid boid2{20.,20.,1.,1.};
-        std::vector<boid> flockvect={boid1,boid2};
-        boids_flock flock{2,flockvect,10.,2.,0.,0.,0.};
-        CHECK(flock.upper_distance(boid1,boid2)==false);
-    }
-    SUBCASE("second boid just farther than upper distance") {
-        boid boid2{10.,1.,1.,1.};
-        std::vector<boid> flockvect={boid1,boid2};
-        boids_flock flock{2,flockvect,10.,2.,0.,0.,0.};
-        CHECK(flock.upper_distance(boid1,boid2)==false);
-    }
-    SUBCASE("second boid exactly on upper distance") {
-        boid boid2{0.,10.,1.,1.};
-        std::vector<boid> flockvect={boid1,boid2};
-        boids_flock flock{2,flockvect,10.,2.,0.,0.,0.};
-        CHECK(flock.upper_distance(boid1,boid2)==false);
-    }
-    SUBCASE("second boid closer than upper distance") {
-        boid boid2{4.,3.,1.,1.};
-        std::vector<boid> flockvect={boid1,boid2};
-        boids_flock flock{2,flockvect,10.,2.,0.,0.,0.};
-        CHECK(flock.upper_distance(boid1,boid2)==true);
-    }
+  boid boid1{0., 0., 1., 1.};
+  SUBCASE("second boid way farther than upper distance") {
+    boid boid2{20., 20., 1., 1.};
+    std::vector<boid> flockvect = {boid1, boid2};
+    boids_flock flock{2, flockvect, 10., 2., 0., 0., 0.};
+    CHECK(flock.upper_distance(boid1, boid2) == false);
+  }
+  SUBCASE("second boid just farther than upper distance") {
+    boid boid2{10., 1., 1., 1.};
+    std::vector<boid> flockvect = {boid1, boid2};
+    boids_flock flock{2, flockvect, 10., 2., 0., 0., 0.};
+    CHECK(flock.upper_distance(boid1, boid2) == false);
+  }
+  SUBCASE("second boid exactly on upper distance") {
+    boid boid2{0., 10., 1., 1.};
+    std::vector<boid> flockvect = {boid1, boid2};
+    boids_flock flock{2, flockvect, 10., 2., 0., 0., 0.};
+    CHECK(flock.upper_distance(boid1, boid2) == false);
+  }
+  SUBCASE("second boid closer than upper distance") {
+    boid boid2{4., 3., 1., 1.};
+    std::vector<boid> flockvect = {boid1, boid2};
+    boids_flock flock{2, flockvect, 10., 2., 0., 0., 0.};
+    CHECK(flock.upper_distance(boid1, boid2) == true);
+  }
 }
 
 TEST_CASE("testing the separation rule function") {
-    SUBCASE("two boids with same y position") {
-    boid boid1{0.,2.,0.,0.};
-    boid boid2{2.,2.,0.,0.};
-    std::vector<boid> flockvect={boid1,boid2};
-    boids_flock flock{2,flockvect,20.,3.,2.,0.,0.};
-    CHECK(flock.separation_rule_y(boid1)==doctest::Approx(0));
-    CHECK(flock.separation_rule_x(boid1)==doctest::Approx(-4));
-    }
+  SUBCASE("two boids with same y position") {
+    boid boid1{0., 2., 0., 0.};
+    boid boid2{2., 2., 0., 0.};
+    std::vector<boid> flockvect = {boid1, boid2};
+    boids_flock flock{2, flockvect, 20., 3., 2., 0., 0.};
+    CHECK(flock.separation_rule_y(boid1) == doctest::Approx(0));
+    CHECK(flock.separation_rule_x(boid1) == doctest::Approx(-4));
+  }
 
-    SUBCASE("two boids with same x position") {
-    boid boid1{2.,0.,0.,0.};
-    boid boid2{2.,2.,0.,0.};
-    std::vector<boid> flockvect={boid1,boid2};
-    boids_flock flock{2,flockvect,20.,3.,2.,0.,0.};
-    CHECK(flock.separation_rule_y(boid1)==doctest::Approx(-4));
-    CHECK(flock.separation_rule_x(boid1)==doctest::Approx(0));
-    }
+  SUBCASE("two boids with same x position") {
+    boid boid1{2., 0., 0., 0.};
+    boid boid2{2., 2., 0., 0.};
+    std::vector<boid> flockvect = {boid1, boid2};
+    boids_flock flock{2, flockvect, 20., 3., 2., 0., 0.};
+    CHECK(flock.separation_rule_y(boid1) == doctest::Approx(-4));
+    CHECK(flock.separation_rule_x(boid1) == doctest::Approx(0));
+  }
 
-    SUBCASE("two boids with different x and y values") {
-    boid boid1{0.,0.,0.,0.};
-    boid boid2{4.,3.,0.,0.};
-    std::vector<boid> flockvect={boid1,boid2};
-    boids_flock flock{2,flockvect,20.,6.,3.,0.,0.};
-    CHECK(flock.separation_rule_y(boid1)==doctest::Approx(-9));
-    CHECK(flock.separation_rule_x(boid1)==doctest::Approx(-12));
-    }
+  SUBCASE("two boids with different x and y values") {
+    boid boid1{0., 0., 0., 0.};
+    boid boid2{4., 3., 0., 0.};
+    std::vector<boid> flockvect = {boid1, boid2};
+    boids_flock flock{2, flockvect, 20., 6., 3., 0., 0.};
+    CHECK(flock.separation_rule_y(boid1) == doctest::Approx(-9));
+    CHECK(flock.separation_rule_x(boid1) == doctest::Approx(-12));
+  }
 
-    SUBCASE("two boids exactly on lower distance") {
-    boid boid1{0.,0.,0.,0.};
-    boid boid2{4.,3.,0.,0.};
-    std::vector<boid> flockvect={boid1,boid2};
-    boids_flock flock{2,flockvect,20.,5.,3.,0.,0.};
-    CHECK(flock.separation_rule_y(boid1)==doctest::Approx(-9));
-    CHECK(flock.separation_rule_x(boid1)==doctest::Approx(-12));
-    }
+  SUBCASE("two boids exactly on lower distance") {
+    boid boid1{0., 0., 0., 0.};
+    boid boid2{4., 3., 0., 0.};
+    std::vector<boid> flockvect = {boid1, boid2};
+    boids_flock flock{2, flockvect, 20., 5., 3., 0., 0.};
+    CHECK(flock.separation_rule_y(boid1) == doctest::Approx(-9));
+    CHECK(flock.separation_rule_x(boid1) == doctest::Approx(-12));
+  }
 }
